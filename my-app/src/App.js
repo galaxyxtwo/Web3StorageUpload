daed80ed14e59e0375313c0f5c43c30c93afc51c
Alex Mirran <90220293+galaxyxtwo@users.noreply.github.com>
2023-08-07 13:01:04 -0500

    added array to save CID list with gateway links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@
 import React, { useState } from 'react';
 import DragAndDrop from './DragAndDrop';
 import { getStorageClient } from './Web3Client';
+import './styles.css';
 
 function App() {
   const [file, setFile] = useState(null);
   const [uploadStatus, setUploadStatus] = useState('Not started');
+  const [cids, setCids] = useState([]);
 
   async function uploadFile() {
     setUploadStatus('Uploading...');
@@ -13,17 +15,32 @@ function App() {
     const cid = await client.put([file]);
     console.log('Uploaded:', cid);
     setUploadStatus('Uploaded');
+    setCids([...cids, cid]);
   }
 
   return (
     <div className="App">
       <DragAndDrop setFile={setFile} />
-      {file && <p>Selected file: {file.name}</p>}
-      <div style={{height: '20px', width: '20px', backgroundColor: uploadStatus === 'Uploaded' ? 'green' : 'red' }} />
-      <button onClick={uploadFile}>Upload to IPFS</button>
+      {file && (
+        <div className="file-info">
+          <p>Selected file: {file.name}</p>
+          <div className={`status-box ${uploadStatus === 'Uploaded' ? 'uploaded' : 'not-uploaded'}`} />
+        </div>
+      )}
+      <button className="upload-button" onClick={uploadFile}>
+        Upload to IPFS
+      </button>
+      <ul className="cid-list">
+        {cids.map((cid, index) => (
+          <li key={index}>
+            <a href={`https://dweb.link/ipfs/${cid}`} target="_blank" rel="noopener noreferrer">
+              {cid}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
